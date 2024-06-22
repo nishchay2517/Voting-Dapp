4480181f7b4f79a635a2c5319345a9df7de77b1b
nishchay2517 <nishchayvalmiki25@gmail.com>
2024-06-22 14:05:19 +0530

    compiled the smart contract success

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomicfoundation/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.24",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+}
---
+++ ignition/modules/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  try {
+      const { ethers } = require("hardhat");
+      const voting = await ethers.getContractFactory("Voting");
+
+      const voting_ = await voting.deploy(["bagri", "vikas", "hemant", "abhishek"], 120);
+
+      console.log("Contract Address:", voting_.address);
+  } catch (error) {
+      console.error("Error deploying contract:", error);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+      console.error("Script error:", error);
+      process.exit(1);
+  });
